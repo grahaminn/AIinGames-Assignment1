05ad637a575176b7d2df1a7e207dc6f56c704365
grahaminn <grahaminnocent@gmail.com>
2023-10-29 20:56:05 +0000

    working bayesian UCB

--- src/main/java/groupM/players/mcts/MCTSEnums.java
+++ src/main/java/groupM/players/mcts/MCTSEnums.java
@@ -3,6 +3,6 @@ package groupM.players.mcts;
 public class MCTSEnums {
         /** Determins the Tree Policy to handle the explore-exploit dilema*/
     public enum ExplorationStrategy{
-        UCB1, Thompson
+        UCB1, Thompson, BayesUCB
     }
 }
